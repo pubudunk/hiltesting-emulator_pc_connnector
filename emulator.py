41a403515e5fa63da0075b6b46b758192a5c58ab
Pubudu Karunaratna <pubudu1@gmail.com>
2024-08-06 01:49:24 -0500

    feat: Add serial communication to emulator
    - Create command packet
    - Sending the packet to emulator and receive reply
    - Validating the reply packet and generate the results
    - TODO: reply is currently mocked until emulator firmware is complete

---
+++ emulator.py
@@ -0,0 +1,239 @@
+import serial
+import traceback
+
+
+#definitions
+UART_TIMEOUT = 60 
+
+
+#constants
+STX = 0x02
+
+PACKET_TYPE_TESTS  = 0x01
+PACKET_TYPE_RESULTS = 0x02
+
+TEST_ID_01 = 0x01
+TEST_ID_02 = 0x02
+TEST_ID_03 = 0x03
+TEST_ID_04 = 0x04
+TEST_ID_05 = 0x05
+
+RESULT_PASS = 0x01
+RESULT_FAIL = 0x02
+RESULT_NA   = 0x03
+
+#global variables
+serialPort = None
+
+def init_serial(port):
+    """
+    Initialize the serial port. Timeout is set to the value of UART_TIMEOUT (seconds)
+
+    Args:
+        port (str): The COM port to use for serial communication (e.g., 'COM6').
+
+    Returns:
+        serial.Serial: The initialized serial port object if successful, None otherwise.
+    """
+
+
+    global serialPort
+
+    try:
+        serialPort = serial.Serial(port, baudrate=115200, timeout=UART_TIMEOUT)
+    except Exception as e:
+        print(f'Exception: opening {port}: {e}')
+
+
+def calculate_crc(data):
+    """
+    Calculate the CRC for a given data packet.
+
+    Args:
+        data (bytes): The data packet for which to calculate the CRC.
+
+    Returns:
+        int: The calculated CRC value.
+    """
+
+
+    crc = 0
+    for byte in data:
+        crc ^= byte
+    
+    return crc
+
+def build_command_packet(packet_type, test_cases):
+    """
+    Build a packet to send over UART using the updated protocol.
+
+    Args:
+        packet_type (int): The type of the packet (0x01 for Tests)
+        test_cases (list): A list of test cases, where each test case is a dictionary
+                           with keys 'id', 'len', and 'val'.
+
+    Returns:
+        bytes: The constructed packet.
+    """
+
+
+    packet = bytearray()
+
+    packet.append(STX)  #STX
+
+    length = 2 # For now use the lenght as packet_type and num_tlvs (which is fixed)
+    packet.append(length)       # packet lenght
+    packet.append(packet_type)  # packet type
+
+    num_tlvs = 0    #For now until total tests are processed
+    packet.append(num_tlvs)  # num of tlvs
+
+    for test in test_cases:
+        packet.append(test['id']) # test id
+        length += 1
+
+        if(test['len'] > 0):
+            #TODO: implement variable length tests
+            pass
+        else:
+            packet.append(0)    # len, data is always zero
+            length += 1
+
+        num_tlvs += 1
+
+    packet[1] = length + 1 # additional byte for CRC
+    packet[3] = num_tlvs 
+
+    crc = calculate_crc(packet)
+    packet.append(crc)
+
+    print('Created command packet:', ' '.join(f'{byte:02X}' for byte in packet))
+
+    return packet
+
+
+def send_tests(test_cases):
+    """
+    Send test cases to the emulator
+
+    Args:
+        test_cases (list): List of test cases to be sent. Each test case is a dictionary
+                            with keys 'id', 'len', 'data' (relating to TLV format) and 'name'
+
+    Returns:
+        Test results response if sucess, None otherwise
+    """
+
+
+    global serialPort
+
+    if serialPort is None:
+        raise Exception("Serial port not initialized")
+    
+    command_packet = build_command_packet(PACKET_TYPE_TESTS, test_cases)
+
+    try:
+        # bytes_written = serialPort.write(command_packet)
+
+        header = bytes([0x02, 0x12]) #TODO mock code remove later
+        reply_data = bytearray([0x02, 0x05, 0x01, 0x01, 0x01, 0x02, 0x01, 0x02, 0x03, 0x01, 0x03, 0x04, 0x01, 0x02, 0x05, 0x01, 0x01]) #TODO mock code remove later
+        reply_data.append(calculate_crc(header+reply_data))
+
+        #if(bytes_written != len(command_packet)):
+        #    print("Error: Not all bytes were written to the serial port. command_packet_len: {len(command_packet)} bytes_written:{bytes_written}")
+        #     return None
+        
+        # # Wait for header (STX + Length + Packet Type)
+        # header = serialPort.read(2)
+        # if (len(header) < 3) or (header[0] != STX):
+        #     print('Error: Invalid or incomplete header received')
+        #     return None
+
+        # length = header[1]
+        # # Read the remainder of the packet
+        # reply_data = serialPort.read(length)
+
+        # if (len(reply_data) < length):
+        #     print(f'Error: Incomplete packet received or timedout received_len: {len(reply_data)}')
+        #     return None
+
+        packet_type = reply_data[0]
+
+        if (packet_type != PACKET_TYPE_RESULTS):
+            print('Error: Invalid packet type received')
+            return None
+        
+        # Validate CRC
+        reply_packet = header + reply_data[:-1]  # excluding CRC
+        received_crc = reply_data[-1]
+        if (received_crc != calculate_crc(reply_packet)):
+            print('Error: Reply packet CRC error')
+            return None
+        
+        print('mock reply packet:', ' '.join(f'{byte:02X}' for byte in bytes(header + reply_data)))
+
+        return bytes(header + reply_data)
+
+    
+    except serial.SerialException as e:
+        print(f'Exception: Serial communication error: {e}')
+        traceback.print_exc()
+        return None
+    
+
+def process_reply_packet(reply_packet):
+    """
+    Process the reply packet from the hardware emulator.
+
+    Args:
+        reply_packet (bytes): The reply packet received from the emulator.
+
+    Returns:
+        list: A list of results, where each result is a dictionary with keys 'id', 'result', 
+              'len' and 'data'
+    """
+
+
+    # Check for STX
+    if not reply_packet or reply_packet[0] != STX:  
+        print("Error: Invalid response received")
+        return []
+    
+    # Check for Results packet type
+    packet_type = reply_packet[2]
+    if packet_type != PACKET_TYPE_RESULTS:  
+        print("Error: Invalid packet type received")
+        return []
+
+    # Check packet length
+    packet_len = reply_packet[1]
+    if((packet_len + 2) != len(reply_packet)): # packet lenght does not include STX and lenght byte
+        print("Error: Invalid packet length")
+        return []
+    
+    num_tlvs = reply_packet[3]
+    results = []
+    index = 4
+
+    # Parse the packet for tlvs and populate the results list of dictionaries
+    for _ in range(num_tlvs):
+        pass
+
+        id = reply_packet[index]
+        lenght = reply_packet[index + 1]
+        result = reply_packet[index + 2]
+
+        if(lenght > 1):
+            #TODO: Reply has additional data. Handle this case later
+            pass
+
+        results.append({
+            'id' : id,
+            'len': lenght,
+            'data': 0x00,
+            'result': result
+        })  
+        
+        index += (2 + lenght)  # tag + len + data length
+
+    return results
--- emulator_pc_connector.py
+++ emulator_pc_connector.py
@@ -7,6 +7,9 @@ import pickle
 import os
 from boto3.session import Session
 from botocore.exceptions import NoCredentialsError, PartialCredentialsError
+import emulator
+import sys
+import traceback
 
 # Configuration
 identity_pool_id = 'us-east-1:128a3c5b-f4b0-45e7-bee4-4302060554ab'
@@ -18,6 +21,18 @@ lambda_function_name = 'hiltesting-websocket-web-client-update-handler'
 credentials_file = 'aws_credentials.pkl'
 
 def assume_role_with_web_identity():
+    """
+    Assumes a role with web identity using AWS Cognito to get temporary credentials.
+
+    This function checks if valid credentials are available in a local file. If not, it fetches
+    a new Cognito Identity ID and obtains temporary credentials for that identity. The credentials
+    are then saved to the file for future use.
+
+    Returns:
+        dict: A dictionary containing AWS credentials including `identity_id`, `access_key_id`,
+              `secret_access_key`, `session_token`, and `expiration`.
+    """
+        
     cognito_client = boto3.client('cognito-identity', region_name=region_name)
     
     if os.path.exists(credentials_file):
@@ -59,7 +74,20 @@ def assume_role_with_web_identity():
     
     return credentials_dict
 
-def get_boto3_client(service_name, credentials):
+
+def get_boto3_client(service_name, credentials):    
+    """
+    Creates a Boto3 client for a specified AWS service using temporary credentials.
+
+    Args:
+        service_name (str): The name of the AWS service to create a client for (e.g., 'dynamodb', 'sqs', 'lambda').
+        credentials (dict): A dictionary containing AWS credentials including `access_key_id`,
+                             `secret_access_key`, and `session_token`.
+
+    Returns:
+        boto3.client: A Boto3 client for the specified AWS service.
+    """
+
     session = Session(
         aws_access_key_id=credentials['access_key_id'],
         aws_secret_access_key=credentials['secret_access_key'],
@@ -68,12 +96,48 @@ def get_boto3_client(service_name, credentials):
     )
     return session.client(service_name)
 
+
+def update_results(results, test_cases):
+    """
+    Updates the results list of dictionaries to add the test case name string
+
+    Args:
+        results (list): List of test results received from emulator. Each test result is a dictionary
+                            with keys 'id', 'len', 'data' and 'result'
+        test_cases (list): List of test cases used for exection. Each test case is a dictionary
+                            with keys 'id', 'len', 'data' (relating to TLV format) and 'name'
+
+    Returns:
+        boto3.client: Updated results list of dictionaries
+    """
+    id_to_name = {int(test['id']): test['name'] for test in test_cases}
+
+    for result in results:
+        result_id = result['id']
+        result['name'] = id_to_name.get(result_id, "Unknown")  # Default to "Unknown" if ID not found
+
+    return results
+
 def process_test_case(test_case):
     # Simulate test execution
     time.sleep(2)  # Simulate 2 seconds of testing
     return random.choice(['pass', 'fail', 'not-executed'])
 
 def main():
+    """
+    Main function to start the test emulator.
+
+    Initializes the serial port, retrieves AWS credentials, sets up AWS service clients,
+    and processes test cases retrieved from an SQS queue. The results of the tests are stored
+    in DynamoDB, and a Lambda function is invoked to notify AWS of the test results.
+    """
+
+
+    print('Test emulator starting...')
+
+    # Initialize serial port
+    ser = emulator.init_serial(sys.argv[1])
+
     try:
         credentials = assume_role_with_web_identity()
 
@@ -81,7 +145,7 @@ def main():
         sqs = get_boto3_client('sqs', credentials)
         lambda_client = get_boto3_client('lambda', credentials)
 
-        print('waiting for tests...')
+        print('Waiting for tests...')
 
         while True:
             # Poll SQS for new messages
@@ -99,28 +163,26 @@ def main():
                 body = json.loads(message['Body'])
                 test_execution_id = body.get('testExecutionId', '')
 
-                print(f'new test execution request received id: {test_execution_id}')
+                print(f'New test execution request received id: {test_execution_id}')
 
                 # Retrieve test cases from DynamoDB
                 response = dynamodb.get_item(
                     TableName=table_name_test_cases,
                     Key={'testExecutionId': {'S': test_execution_id}}
                 )
-                tests = json.loads(response.get('Item', {}).get('tests', {}).get('S', '[]'))
+                tests_json = response.get('Item', {}).get('tests', {}).get('S', '[]')
+                tests = json.loads(tests_json)
 
-                print(f'executing tests: {tests}')
+                test_cases = [{'id': test['id'], 'len': test.get('len', ''), 'data': test.get('data', ''), 'name': test.get('name', '')} for test in tests]
 
-                # Execute tests
-                results = []
-                for test in tests:
-                    test_result = process_test_case(test)
-                    results.append({
-                        'id': test['id'],
-                        'name': test['name'],
-                        'result': test_result
-                    })
-
-                print(f'test results: {results}')
+                print(f'Executing tests: {tests}')
+                #print(f'executing tests: {test_cases}')
+                
+                reply_packet = emulator.send_tests(test_cases)
+                results = emulator.process_reply_packet(reply_packet)
+                results = update_results(results, test_cases)
+                           
+                print(f'Test results: {results}')
 
                 # Save test results to DynamoDB
                 dynamodb.put_item(
@@ -149,8 +211,10 @@ def main():
 
     except (NoCredentialsError, PartialCredentialsError) as e:
         print(f"Error obtaining temporary credentials: {e}")
+        traceback.print_exc()
     except Exception as e:
         print(f"An error occurred: {e}")
+        traceback.print_exc()
 
 if __name__ == "__main__":
     main()
